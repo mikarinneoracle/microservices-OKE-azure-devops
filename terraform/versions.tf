4c2e190acebffd9e5cb101773a27aa94b0e487b7
Mika Rinne <mika.rinne@oracle.com>
2024-02-21 13:25:03 +0000

    Added versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">=4.93.0"
+    }
+  }
+  backend "http" {
+    address = "https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/q-9i-3q__W7TSVv-9OXoc4XMHkfYTc1vcn1xWvOki1hv1fhKT1qnBUe9T8MYpacQ/n/frsxwtjslf35/b/tf-state/o/"
+    update_method = "PUT"
+  }
+}
+
+provider "oci" {
+   auth = "InstancePrincipal"
+   region = "eu-frankfurt-1"
+}
