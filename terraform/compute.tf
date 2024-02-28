a426df42ef007f1cb14167c6f7ca42ce0048ae79
Mika Rinne <mika.rinne@oracle.com>
2024-02-28 10:38:27 +0000

    Updated compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -7,6 +7,15 @@ resource "oci_core_instance" "demo_vm" {
     Managed = var.tags
   }
   
+resource "oci_core_instance" "demo_vm_2" {
+  availability_domain = lookup(data.oci_identity_availability_domains.this.availability_domains[0], "name")
+  compartment_id      = var.compartment_id
+  display_name        = "demo2"
+  shape               = var.demo_shape
+  freeform_tags = {
+    Managed = var.tags
+  }
+
   shape_config {
     baseline_ocpu_utilization = "BASELINE_1_1"
     memory_in_gbs     = var.demo_shape_mem
