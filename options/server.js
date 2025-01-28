be602b5ef085ceb640690be0c44d05e59c047207
Mika Rinne <mika.rinne@oracle.com>
2025-01-28 10:28:35 +0200

    fixes

--- options/server.js
+++ options/server.js
@@ -17,7 +17,7 @@ async function init() {
     await oracledb.createPool({
       user: "admin",
       password: password,
-      connectString: "10.96.139.108:1521/MYATP",
+      connectString: "db23ai-clusterip:1521/MYATP",
       // edition: 'ORA$BASE', // used for Edition Based Redefintion
       // events: false, // whether to handle Oracle Database FAN and RLB events or support CQN
       // externalAuth: false, // whether connections should be established using External Authentication
